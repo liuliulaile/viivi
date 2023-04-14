598f3845435c74e34504fbfb88c7bc2a40cc4d18
= <=>
2023-04-14 21:17:34 +0800

    Liu.First update.

---
+++ src/component/homepage.js
@@ -0,0 +1,15 @@
+// src/components/Homepage.js
+import React from 'react';
+
+class Homepage extends React.Component{
+    render(){
+        return (
+            <div>
+                i am home component.
+            </div>
+        )
+    }
+}
+
+export default Homepage;
+
